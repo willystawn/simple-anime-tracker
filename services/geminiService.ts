33df9b0096c374f6f34b07546a2f0637003df2f2
Willy Setiawan <willystawn@gmail.com>
2025-07-09 00:00:02 +0700

    update key

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 import { Recommendation } from '../types';
 
-const apiKey = process.env.API_KEY;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const ai = apiKey ? new GoogleGenAI({ apiKey: apiKey }) : null;
 
